fe984dbedd1a29c558b7ec419af160560346b94d
Damien Le Berrigaud <damien@itsdamo.com>
2017-02-01 14:45:40 -0700

    Specify return types.

--- components/jdbc-support/src/main/java/com/somanyfeeds/jdbcsupport/ResultSetExtensions.kt
+++ components/jdbc-support/src/main/java/com/somanyfeeds/jdbcsupport/ResultSetExtensions.kt
@@ -1,8 +1,12 @@
 package com.somanyfeeds.jdbcsupport
 
 import java.sql.ResultSet
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 
-fun ResultSet.getLocalDate(index: Int) = getDate(index).toLocalDate()
+fun ResultSet.getLocalDate(index: Int): LocalDate
+    = getDate(index).toLocalDate()
 
-fun ResultSet.getLocalDateTime(index: Int) = getTimestamp(index).toLocalDateTime()
+fun ResultSet.getLocalDateTime(index: Int): LocalDateTime
+    = getTimestamp(index).toLocalDateTime()
