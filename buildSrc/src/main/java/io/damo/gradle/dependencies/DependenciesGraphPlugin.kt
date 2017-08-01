cf443105c599b82134c82cf00d1f215350f2298e
Damien Le Berrigaud <damien@itsdamo.com>
2017-08-01 11:39:56 -0400

    Extract dependencies graph plugin.

---
+++ buildSrc/src/main/java/io/damo/gradle/dependencies/DependenciesGraphPlugin.kt
@@ -0,0 +1,57 @@
+package io.damo.gradle.dependencies
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.artifacts.UnknownConfigurationException
+import org.gradle.api.tasks.Exec
+import org.gradle.script.lang.kotlin.get
+import org.gradle.script.lang.kotlin.task
+
+class DependenciesGraphPlugin : Plugin<Project> {
+
+    override fun apply(project: Project): Unit = with(project) {
+        task("dependenciesGraphDot") {
+            group = "DependenciesGraph"
+            description = "Generate DOT file"
+
+            mustRunAfter("clean")
+
+            doLast {
+                val graphBuildDir = "$buildDir/dependenciesGraph"
+
+                delete(graphBuildDir)
+                mkdir(graphBuildDir)
+
+                var dotFileText = ""
+                dotFileText += "digraph dependencies {\n"
+
+                subprojects.forEach { subProject ->
+                    try {
+                        val compileConfig = subProject.configurations["compile"]
+                        compileConfig
+                            .dependencies
+                            .filter { it is ProjectDependency }
+                            .forEach {
+                                val projectDependency = it as ProjectDependency
+                                dotFileText += "  \"${subProject.name}\" -> \"${projectDependency.dependencyProject.name}\"\n"
+                            }
+                    } catch (ignored: UnknownConfigurationException) {
+                    }
+                }
+
+                dotFileText += "}\n"
+                file("$graphBuildDir/graph.dot").writeText(dotFileText)
+            }
+        }
+
+        task<Exec>("dependenciesGraph") {
+            group = "DependenciesGraph"
+            description = "Generate PNG file"
+
+            dependsOn("dependenciesGraphDot")
+            workingDir("$buildDir/dependenciesGraph")
+            commandLine("dot", "-O", "-Tpng", "graph.dot")
+        }
+    }
+}
